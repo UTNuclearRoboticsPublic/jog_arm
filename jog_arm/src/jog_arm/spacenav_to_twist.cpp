56b680b9bfa61e7d9cea68f1451af90581691d58
AndyZe <andyz@utexas.edu>
2018-07-24 09:14:45 -0500

    Add joint jogging to the SpaceNav and XBox demos.

--- jog_arm/src/jog_arm/spacenav_to_twist.cpp
+++ jog_arm/src/jog_arm/spacenav_to_twist.cpp
@@ -1,4 +1,5 @@
 #include "geometry_msgs/TwistStamped.h"
+#include "jog_msgs/JogJoint.h"
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
 
@@ -11,6 +12,7 @@ public:
   {
     joy_sub_ = n_.subscribe("spacenav/joy", 1, &spaceNavToTwist::joyCallback, this);
     twist_pub_ = n_.advertise<geometry_msgs::TwistStamped>("jog_arm_server/delta_jog_cmds", 1);
+    joint_delta_pub_ = n_.advertise<jog_msgs::JogJoint>("jog_arm_server/joint_delta_jog_cmds", 1);
 
     spinner_.start();
     ros::waitForShutdown();
@@ -19,23 +21,35 @@ public:
 private:
   ros::NodeHandle n_;
   ros::Subscriber joy_sub_;
-  ros::Publisher twist_pub_;
+  ros::Publisher twist_pub_, joint_delta_pub_;
   ros::AsyncSpinner spinner_;
 
-  // Convert incoming joy commands to TwistStamped commands for jogging
+  // Convert incoming joy commands to TwistStamped commands for jogging.
+  // The TwistStamped component goes to jogging, while buttons 0 & 1 control joints directly.
   void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
   {
-    geometry_msgs::TwistStamped t_s;
-    t_s.header.stamp = ros::Time::now();
-    t_s.twist.linear.x = msg->axes[0];
-    t_s.twist.linear.y = msg->axes[1];
-    t_s.twist.linear.z = msg->axes[2];
+    // Cartesian jogging with the axes
+    geometry_msgs::TwistStamped twist;
+    twist.header.stamp = ros::Time::now();
+    twist.twist.linear.x = msg->axes[0];
+    twist.twist.linear.y = msg->axes[1];
+    twist.twist.linear.z = msg->axes[2];
 
-    t_s.twist.angular.x = msg->axes[3];
-    t_s.twist.angular.y = msg->axes[4];
-    t_s.twist.angular.z = msg->axes[5];
+    twist.twist.angular.x = msg->axes[3];
+    twist.twist.angular.y = msg->axes[4];
+    twist.twist.angular.z = msg->axes[5];
 
-    twist_pub_.publish(t_s);
+    // Joint jogging with the buttons
+    jog_msgs::JogJoint joint_deltas;
+    // This example is for a Motoman SIA5. joint_s is the wrist joint.
+    joint_deltas.joint_names.push_back("joint_s");
+
+    // Button 0: positive on the wrist joint
+    // Button 1: negative on the wrist joint
+    joint_deltas.deltas.push_back( msg->buttons[0] - msg->buttons[1] );
+
+    twist_pub_.publish(twist);
+    joint_delta_pub_.publish(joint_deltas);
   }
 };
 }  // end to_twist namespace
--- jog_arm/src/jog_arm/xbox_to_twist.cpp
+++ jog_arm/src/jog_arm/xbox_to_twist.cpp
@@ -1,4 +1,5 @@
 #include "geometry_msgs/TwistStamped.h"
+#include "jog_msgs/JogJoint.h"
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
 
@@ -11,6 +12,7 @@ public:
   {
     joy_sub_ = n_.subscribe("joy", 1, &xboxToTwist::joyCallback, this);
     twist_pub_ = n_.advertise<geometry_msgs::TwistStamped>("jog_arm_server/delta_jog_cmds", 1);
+    joint_delta_pub_ = n_.advertise<jog_msgs::JogJoint>("jog_arm_server/joint_delta_jog_cmds", 1);
 
     spinner_.start();
     ros::waitForShutdown();
@@ -19,27 +21,37 @@ public:
 private:
   ros::NodeHandle n_;
   ros::Subscriber joy_sub_;
-  ros::Publisher twist_pub_;
+  ros::Publisher twist_pub_, joint_delta_pub_;
   ros::AsyncSpinner spinner_;
 
   // Convert incoming joy commands to TwistStamped commands for jogging
   void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
   {
-    geometry_msgs::TwistStamped t_s;
-    t_s.header.stamp = ros::Time::now();
-
+    // Cartesian jogging
+    geometry_msgs::TwistStamped twist;
+    twist.header.stamp = ros::Time::now();
     // This button is binary
-    t_s.twist.linear.x = -msg->buttons[4] + msg->buttons[5];
+    twist.twist.linear.x = -msg->buttons[4] + msg->buttons[5];
     // Double buttons
-    t_s.twist.linear.y = msg->axes[0];
-    t_s.twist.linear.z = msg->axes[1];
-
-    t_s.twist.angular.x = -msg->axes[3];
-    t_s.twist.angular.y = msg->axes[4];
+    twist.twist.linear.y = msg->axes[0];
+    twist.twist.linear.z = msg->axes[1];
+    twist.twist.angular.x = -msg->axes[3];
+    twist.twist.angular.y = msg->axes[4];
     // A binary button
-    t_s.twist.angular.z = -msg->buttons[0] + msg->buttons[1];
+    twist.twist.angular.z = -msg->buttons[0] + msg->buttons[1];
 
-    twist_pub_.publish(t_s);
+
+    // Joint jogging
+    jog_msgs::JogJoint joint_deltas;
+    // This example is for a Motoman SIA5. joint_s is the wrist joint.
+    joint_deltas.joint_names.push_back("joint_s");
+    // Button : positive on the wrist joint
+    // Button : negative on the wrist joint
+    joint_deltas.deltas.push_back( msg->buttons[6] - msg->buttons[7] );
+
+
+    twist_pub_.publish(twist);
+    joint_delta_pub_.publish(joint_deltas);
   }
 };
 }  // end to_twist namespace
